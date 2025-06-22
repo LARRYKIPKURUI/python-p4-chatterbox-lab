05e9472247c8e4e7fbfe9379608a0012cf9f9516
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-06-22 13:11:05 +0300

    Creating endpoints for frontend

--- server/app.py
+++ server/app.py
@@ -13,14 +13,57 @@ CORS(app)
 migrate = Migrate(app, db)
 
 db.init_app(app)
-
-@app.route('/messages')
+# GET & POST /messages
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at).all()
+        print("Fetched messages:", messages)
 
+        return jsonify([message.to_dict() for message in messages]), 200
+
+    elif request.method == 'POST':
+        data = request.get_json()
+
+        new_message = Message(
+            body=data["body"],
+            username=data["username"],
+        )
+        db.session.add(new_message)
+        db.session.commit()
+
+        return jsonify(new_message.to_dict()), 201  # 201 for created
+
+
+# GET /messages/<id>
 @app.route('/messages/<int:id>')
 def messages_by_id(id):
-    return ''
+    message = Message.query.get(id)
+    if not message:
+        return jsonify({"error": "Message not found"}), 404
+    return jsonify(message.to_dict()), 200
+
+
+# PATCH /messages/<id>
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    msg = Message.query.get_or_404(id)
+    data = request.get_json()
+    if not data or 'body' not in data:
+        return jsonify({"error": "body required"}), 400
+
+    msg.body = data['body']
+    db.session.commit()
+    return jsonify(msg.to_dict()), 200
+
+
+# DELETE /messages/<id>
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    msg = Message.query.get_or_404(id)
+    db.session.delete(msg)
+    db.session.commit()
+    return jsonify({"message": "Message deleted successfully."}), 200
 
 if __name__ == '__main__':
     app.run(port=5555)
